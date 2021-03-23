905ce3297fc9d96e6ef4dc76448ac5e5d61fc06a
vadiminsk <vadiminsk90@gmail.com>
2021-03-23 13:23:49 +0300

    install bootstrap, add styles and context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Play from './components/Play';
+import Start from './components/Start';
+import { MyContext } from './context';
 
-const App = () => {
-  return <div></div>;
-};
+class App extends React.Component {
+  static contextType = MyContext;
+
+  render() {
+    return (
+      <div className='wrapper'>
+        <div className='center-wrapper'>
+          <h1>Who pays the bill?</h1>
+          {this.context.state.stage === 1 ? <Start /> : <Play />}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
